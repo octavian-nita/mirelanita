95e2a9deaea3c66d0f70325e1c20c4362a7bee74
Octavian Nita <octavian.nita@gmail.com>
2017-03-08 16:30:41 +0100

    simpler Dimension, better SRP

--- src/main/java/com/mirelanita/support/im/Dimension.java
+++ src/main/java/com/mirelanita/support/im/Dimension.java
@@ -1,12 +1,6 @@
 package com.mirelanita.support.im;
 
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static java.lang.Double.compare;
-import static java.lang.Integer.parseInt;
-import static java.lang.Math.round;
 
 /**
  * @author Octavian Theodor Nita (https://github.com/octavian-nita/)
@@ -14,23 +8,9 @@ import static java.lang.Math.round;
  */
 public class Dimension implements Comparable<Dimension> {
 
-    public static final String REGEX = "\\s*-?\\s*([0-9]+)\\s*[xX]\\s*([0-9]+)\\s*-?\\s*";
-
-    private static final Pattern PATTERN = Pattern.compile(REGEX); // no need to compile this every time...
-
     private final int width;  // pixels or no unit for now...
 
     private final int height; // pixels or no unit for now...
-
-    public Dimension(String spec) {
-        final Matcher matcher = PATTERN.matcher(spec);
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException("Dimension specification must follow the <integer>x<integer> syntax");
-        }
-
-        this.width = parseInt(matcher.group(1));
-        this.height = parseInt(matcher.group(2));
-    }
 
     public Dimension(int side) { this(side, side); }
 
@@ -49,33 +29,27 @@ public class Dimension implements Comparable<Dimension> {
 
     public int getHeight() { return height; }
 
-    public Dimension fit(String bounds) { return fit(new Dimension(bounds)); }
-
     public Dimension fit(Dimension bounds) {
-        Objects.requireNonNull(bounds, "cannot fit a Dimension within null bounds");
-
-        if (bounds.height < height || bounds.width < width) {
-            final double ratio = width / height;
-
-            if (bounds.height > bounds.width) {
-                int newHeight = (int) round(bounds.width / ratio); // keep width, re-compute height
-                return new Dimension(width, newHeight == 0 ? 1 : newHeight);
-            } else {
-                int newWidth = (int) round(bounds.height * ratio); // keep height, re-compute width
-                return new Dimension(newWidth == 0 ? 1 : newWidth, height);
-            }
-        } else {
-            return this;
-        }
+        return bounds == null
+               ? this
+               : scale(Double.min((double) bounds.width / width, (double) bounds.height / height));
     }
 
+    public Dimension fit(int width, int height) {
+        return width <= 0 || height <= 0 ? this : fit(new Dimension(width, height));
+    }
+
+    public Dimension fitWidth(int width) { return width <= 0 ? this : fit(new Dimension(width, height)); }
+
+    public Dimension fitHeight(int height) { return height <= 0 ? this : fit(new Dimension(width, height)); }
+
     public Dimension scale(double factor) {
-        if (compare(factor, 0.) <= 0) {
-            throw new IllegalArgumentException("cannot scale a Dimension by less than or equal to 0");
+        if (Double.compare(factor, 0.) <= 0) {
+            return this;
         }
 
-        int newWidth = (int) round(width * factor);
-        int newHeight = (int) round(height * factor);
+        int newWidth = (int) Math.round(width * factor);
+        int newHeight = (int) Math.round(height * factor);
 
         return new Dimension(newWidth == 0 ? 1 : newWidth, newHeight == 0 ? 1 : newHeight);
     }
@@ -94,9 +68,4 @@ public class Dimension implements Comparable<Dimension> {
 
     @Override
     public String toString() { return width + "x" + height; }
-
-    public static void main(String[] args) {
-        Dimension d = new Dimension("2560x1600");
-        System.out.println(d.fit("2400x1500"));
-    }
 }
