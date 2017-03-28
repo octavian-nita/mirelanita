7d73f51190abbf136f2f052df3dbf74744e71a31
Octavian Nita <octavian.nita@gmail.com>
2017-03-28 18:39:23 +0200

    finding files, oo-style

--- src/main/java/com/mirelanita/support/io/FileSet.java
+++ src/main/java/com/mirelanita/support/io/FileSet.java
@@ -1,82 +1,129 @@
 package com.mirelanita.support.io;
 
+import org.apache.commons.collections4.iterators.SingletonIterator;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.DirectoryStream.Filter;
-import java.util.function.Consumer;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import static java.nio.file.Files.isRegularFile;
 import static java.util.Objects.requireNonNull;
+import static org.apache.commons.collections4.iterators.EmptyIterator.emptyIterator;
 
 /**
- * TODO:
+ * Potential enhancements:
  * <ul>
- * <li>parallel processing / glob</li>
+ * <li>finding files in parallel</li>
  * <li>recursive pattern matching</li>
  * </ul>
  *
  * @author Octavian Theodor Nita (https://github.com/octavian-nita/)
  * @version 1.0, Mar 01, 2017
  */
-public class FileSet {
+public class FileSet implements Iterable<Path> {
 
     private final Path root;
 
-    private final String glob;
+    private final String syntaxAndPattern;
 
     public FileSet(String root) { this(root, null); }
 
-    public FileSet(String root, String glob) {
-        this(Paths.get(requireNonNull(root, "the root path of a file set cannot be null")), glob);
+    public FileSet(String root, String syntaxAndPattern) {
+        this(Paths.get(requireNonNull(root, "the root path of a file set cannot be null")), syntaxAndPattern);
     }
 
     public FileSet(File root) { this(root, null); }
 
-    public FileSet(File root, String glob) {
-        this(requireNonNull(root, "the root path of a file set cannot be null").toPath(), glob);
+    public FileSet(File root, String syntaxAndPattern) {
+        this(requireNonNull(root, "the root path of a file set cannot be null").toPath(), syntaxAndPattern);
     }
 
     public FileSet(Path root) { this(root, null); }
 
-    public FileSet(Path root, String glob) {
+    public FileSet(Path root, String syntaxAndPattern) {
         this.root = requireNonNull(root, "the root path of a file set cannot be null");
 
-        if (glob == null || (glob = glob.trim()).length() == 0) {
-            glob = "glob:*";
-        } else if (!glob.startsWith("glob:")) {
-            glob = "glob:" + glob;
+        if (syntaxAndPattern == null || (syntaxAndPattern = syntaxAndPattern.trim()).length() == 0) {
+            syntaxAndPattern = "glob:*";
+        } else if (!syntaxAndPattern.startsWith("glob:") && !syntaxAndPattern.startsWith("regex:")) {
+            syntaxAndPattern = "glob:" + syntaxAndPattern;
         }
-        this.glob = glob;
+        this.syntaxAndPattern = syntaxAndPattern;
     }
 
-    public void forEach(Consumer<? super Path> action) throws IOException {
-        requireNonNull(action);
+    @Override
+    public Iterator<Path> iterator() {
 
         final FileSystem fs = root.getFileSystem();
 
-        DirectoryStream<Path> dirStream;
-        if ("*".equals(glob) || "glob:*".equals(glob)) { // no need for a path matcher...
+        if ("*".equals(syntaxAndPattern) || "glob:*".equals(syntaxAndPattern)) {
 
-            dirStream = fs.provider().newDirectoryStream(root, FLT_ONLY_FILES);
-
-        } else {
-            final PathMatcher matcher = fs.getPathMatcher(glob);
-
-            if (isRegularFile(root)) {
-                if (matcher.matches(root.getFileName())) {
-                    action.accept(root);
-                }
-                return;
+            // no need for a path matcher...
+            try {
+                return new ClosingIterator<>(fs.provider().newDirectoryStream(root, p -> isRegularFile(p)));
+            } catch (IOException e) {
+                LoggerFactory.getLogger(FileSet.class).error("cannot access directory " + root, e);
+                return emptyIterator();
             }
 
-            dirStream =
-                fs.provider().newDirectoryStream(root, p -> isRegularFile(p) && matcher.matches(p.getFileName()));
         }
 
-        dirStream.forEach(action);
+        final PathMatcher matcher = fs.getPathMatcher(syntaxAndPattern);
+        if (isRegularFile(root)) {
+            return matcher.matches(root.getFileName()) ? new SingletonIterator<>(root) : emptyIterator();
+        }
+
+        try {
+            return new ClosingIterator<>(
+                fs.provider().newDirectoryStream(root, p -> isRegularFile(p) && matcher.matches(p.getFileName())));
+        } catch (IOException e) {
+            LoggerFactory.getLogger(FileSet.class).error("cannot access directory " + root, e);
+            return emptyIterator();
+        }
+
     }
 
-    public static final Filter<Path> FLT_ONLY_FILES = path -> isRegularFile(path);
+    protected static class ClosingIterator<E, T extends Closeable & Iterable<E>> implements Iterator<E> {
 
+        private final T delegate;
+
+        private final Iterator<E> iterator;
+
+        protected void close(T delegate) {
+            try {
+                delegate.close();
+            } catch (IOException e) {
+                LoggerFactory.getLogger(ClosingIterator.class).error("cannot close delegate", e);
+            }
+        }
+
+        public ClosingIterator(T delegate) {
+            this.delegate = requireNonNull(delegate);
+            this.iterator = delegate.iterator();
+        }
+
+        @Override
+        public boolean hasNext() { return iterator.hasNext(); }
+
+        @Override
+        public E next() {
+            try {
+                final E next = iterator.next();
+                if (!iterator.hasNext()) {
+                    close(delegate);
+                }
+                return next;
+            } catch (NoSuchElementException nse) {
+                close(delegate);
+                throw nse;
+            }
+        }
+    }
 }
